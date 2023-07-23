2fceb355a7940c7ee706fa86924c4940af9bda7f
hyper-v <tsudaryo@utautattaro.com>
2023-07-23 23:06:50 +0900

    step2

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial/>
+  <b-container>
+    <b-input-group prepend="足し算">
+      <b-form-input type="number" v-model="a"></b-form-input>
+      <b-input-group-prepend is-text><b>＋</b></b-input-group-prepend>
+      <b-form-input type="number" v-model="b"></b-form-input>
+      <b-input-group-append is-text><b>＝</b></b-input-group-append>
+      <b-input-group-append is-text><b>{{parseInt(a,10) + parseInt(b,10)</b></b-input-group-append>
+    </b-input-group>
+  </b-container>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      a: 0,
+      b: 0
+    }
+  }
 }
 </script>
